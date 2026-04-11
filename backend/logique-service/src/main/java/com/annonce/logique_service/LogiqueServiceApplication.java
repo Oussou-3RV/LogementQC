ebb7ed7ad0471c8c5f5e81c6823c307f529b62d6
Oussou-3RV <baldemarc224@gmail.com>
2026-04-10 22:16:40 -0400

    feat(backend): initialisation Spring Boot microservices

---
+++ backend/logique-service/src/main/java/com/annonce/logique_service/LogiqueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.annonce.logique_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiqueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiqueServiceApplication.class, args);
+	}
+
+}
---
+++ backend/logique-service/src/test/java/com/annonce/logique_service/LogiqueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annonce.logique_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiqueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
